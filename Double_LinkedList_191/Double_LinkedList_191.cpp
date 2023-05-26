12cf4df468f6567b3d6afd0e205d44b79bef460d
tedyistiawan <tedypnd07@gmail.com>
2023-05-26 19:50:02 +0700

    3

--- Double_LinkedList_191/Double_LinkedList_191.cpp
+++ Double_LinkedList_191/Double_LinkedList_191.cpp
@@ -9,3 +9,18 @@ public:
 	Node* next;
 	Node* prev;
 };
+
+class DoubleLinkedList {
+private:
+	Node* START;
+public:
+	DoubleLinkedList();
+	void addNode();
+	bool search(int rollNo, Node** previous, Node** current);
+	bool deleteNode(int rollNo);
+	bool listEmpty();
+	void traverse();
+	void revtraverse();
+	void hapus();
+	void searchData();
+};
